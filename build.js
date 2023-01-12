28eed1e519c986634c4effcf4e7e32e1df255459
Mark Anthone <myanthoneluna@gmail.com>
2023-01-13 03:07:29 +0800

    Remove the unused actions called when building the style dictionary css variables, Code cleanup and optimization in the style dictionary integration

--- build.js
+++ build.js
@@ -5,26 +5,26 @@ StyleDictionary.registerFormat({
   formatter: function (dictionary, config) {
     return `
     :root{
-${dictionary.allProperties
-  .map((prop) => {
-    return prop.type === 'typography' ? ``
-    : !isNaN(prop.value) ? `--${prop.name}: ${prop.value}px;`
-    : prop.type === 'spacing' ? `--${prop.name}: ${prop.value.split(' ')[0]}px ${prop.value.split(' ')[1]}px;`
-    : `--${prop.name}: ${prop.value};`
-  })
-  .join('\n')}
-}
-${dictionary.allProperties
-  .map((prop) => {
-    return prop.type === 'typography'  ? `
-    .${prop.name} {
-        font-family: ${prop.value.fontFamily};
-        font-size: ${prop.value.fontSize}px;
-        font-weight: ${prop.value.fontWeight};
-        line-height: ${prop.value.lineHeight};
-    };`: ``})
-  .join('\n')}
-`
+      ${dictionary.allProperties
+        .map((prop) => {
+          return prop.type === 'typography' ? false
+          : !isNaN(prop.value) ? `--${prop.name}: ${prop.value}px;`
+          : prop.type === 'spacing' ? `--${prop.name}: ${prop.value.split(' ')[0]}px ${prop.value.split(' ')[1]}px;`
+          : `--${prop.name}: ${prop.value};`
+        })
+        .join('\n')}
+    }
+    ${dictionary.allProperties
+      .map((prop) => {
+        return prop.type === 'typography'  ? `
+        .${prop.name} {
+            font-family: ${prop.value.fontFamily};
+            font-size: ${prop.value.fontSize}px;
+            font-weight: ${prop.value.fontWeight};
+            line-height: ${prop.value.lineHeight};
+        };`: false})
+      .join('\n')}
+    `
   },
 })
 
